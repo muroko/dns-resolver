1845f4d5fef3f9f80700d282d5491855b6d98189
Muroko <murokomail@gmail.com>
2023-10-15 02:44:14 -0400

    INITIAL: v2

--- README.md
+++ README.md
@@ -1,2 +1,68 @@
-# dns-resolver
-Dnero DNS Resolver - Not fully setup Yet!
+# Dnero Name Service - DNS Resolver
+
+###### This NPM can be used to retrieve an address from a DNS or a DNS from an address on the Dnero Blockchain.
+###### It can also retrieve a list of domain names from a list of addresses.
+
+### Installation
+To install dns-resolver use the following statement:
+```console
+npm install dns-resolver
+```
+
+### Import and instanciate DNS
+To import and instanciate dns-resolver use the following statement:
+```js script
+import DNS from "dns-resolver";
+
+const dns = new DNS();
+```
+
+### Get domain name from an address
+To get the domain name from an address use the ```getDomainName``` function such as: 
+```js script
+import DNS from 'dns-resolver';
+
+const address = '0x123...';
+const dns = new DNS();
+const domainName = await dns.getDomainName(address);
+console.log(domainName);
+/* will return the domain name 
+ or null if no domain name is assigned to this address
+ i.e. will log 'domainname.dnero' or null */
+```
+
+### Get address from a domain name
+To get the address from a domain name use the ```getAddress``` function such as:
+```js script
+import DNS from 'dns-resolver';
+
+const domainName = 'test.dnero';
+const dns = new DNS();
+const address = await dns.getAddress(domainName);
+console.log(address);
+/* will return the address or null if the domain
+is not assigned to an address
+i.e. will log '0x123...' or null */
+```
+
+### Get domain names for a list of addresses
+If you need to process many addresses (eg: showing domain names of transaction histories), use the ```getDomainNames``` function such as: 
+```js script
+import DNS from 'dns-resolver';
+
+const addresses = ['0x123...', '0x456...'];
+const dns = new DNS();
+const domainNames = await dns.getDomainNames(addresses);
+console.log(domainNames);
+/* will return an object where the key is the address
+ and the value is the domain name or an empty string if no domain name is assigned on this address
+ i.e. will log { "0x123...": "domainname.dnero", "0x456...": "" } */
+```
+
+### Instanciate DNS with a custom RPC endpoint
+To instanciate dns-resolver using a custom RPC endpoint use the following statement:
+```js script
+import DNS from "dns-resolver";
+
+const dns = new DNS({customRpcEndpoint: 'YOUR_CUSTOM_RPC_ENDPOINT_HERE'});
+```
---
+++ index.js
@@ -0,0 +1,135 @@
+import { ethers } from "ethers"
+import { namehash, isAddress} from "./utils/utils"
+import { toChecksumAddress } from "ethereum-checksum-address"
+
+const ethNamehash = require('eth-ens-namehash')
+const resolverABI = require("./contracts/PublicResolver.json")
+const reverseRecordsABI = require("./contracts/ReverseRecords.json")
+const EMPTY_ADDRESS = '0x0000000000000000000000000000000000000000'
+const defaultRpc = 'https://eth-rpc-api.dnerochain.xyz/rpc'
+
+
+const getResolvercontract = (provider) => {
+    return new ethers.Contract("0x428101CB7a0D22587a6f098A518c36Af2a1b2E31", resolverABI, provider)
+}
+
+const getReverseRecordsContract = (provider) => {
+    return new ethers.Contract("0x5aA729cB43FDcC26CA947Dfb1Cbe11cc8d245B9C", reverseRecordsABI, provider)
+}
+
+const getAddressRecord = async(domain, provider) => {
+    try {
+        const label = namehash(domain + '.dnero')
+        const resolverContract = getResolvercontract(provider)
+        return await resolverContract['addr(bytes32)'](label)   
+    } catch (e) {
+        console.log(`Error getAddressRecord for resolverContract`, e)
+        return null
+    }
+}
+
+const getReverseName = async(address, provider) => {
+    try {
+        const reverseNode = `${address.slice(2)}.addr.reverse`
+        const reverseNamehash = namehash(reverseNode)
+        const resolverContract = getResolvercontract(provider)
+        return await resolverContract.name(reverseNamehash)
+    } catch (e) {
+        console.log(`Error getReverseName for resolverContract`, e)
+        return null
+    }
+}
+
+const getReverseNames = async(addresses, extension, provider) => {
+    try {
+        if (!extension) extension = 'dnero'
+        const reverseRecordsContract = getReverseRecordsContract(provider)
+        return await reverseRecordsContract.getNames(addresses, extension)
+    } catch (e) {
+        console.log(`Error getReverseNames for reverseRecordsContract`, e)
+        return null
+    }
+}
+
+export default class DNS {
+    constructor(options) {
+        const { customRpcEndpoint } = options || {};
+        let rpcEndpoint = customRpcEndpoint ? customRpcEndpoint : defaultRpc
+        this.provider = new ethers.providers.JsonRpcProvider(rpcEndpoint)
+    }
+
+    //Returns record address of domain if it matches the reversename
+    async getAddress(domain) {
+        try {
+            const formattedDomain = domain.toLowerCase().replace('.dnero', '')
+            const addressRecord = await getAddressRecord(formattedDomain, this.provider)
+            if (addressRecord && addressRecord !== EMPTY_ADDRESS) {
+                const reverseName = await getReverseName(addressRecord, this.provider)
+                if (reverseName && reverseName === formattedDomain) {
+                    return addressRecord
+                }
+            }
+            return null
+        } catch (e) {
+            console.log(`Error getAddress`, e)
+            return null
+        }
+    }
+
+
+    async getDomainNames(addresses, extension) {
+        try {
+            let addressesToReverse = [],
+             result = {},
+             count = 0,
+             reversedNames = []
+            for (const address of addresses) {
+                if (isAddress(address)) {
+                    const checksummedAddress = toChecksumAddress(address)
+                    addressesToReverse.push(checksummedAddress)
+                } else {
+                    addressesToReverse.push(address)
+                }
+                if (count <= 2000) {
+                    count++
+                } else {
+                    const tempreversedNames = await getReverseNames(addressesToReverse, extension, this.provider)
+                    reversedNames.push(...tempreversedNames)
+                    addressesToReverse = []
+                    count = 0
+                }
+            }
+            if (addressesToReverse.length) {
+                const tempreversedNames = await getReverseNames(addressesToReverse, extension, this.provider)
+                reversedNames.push(...tempreversedNames)
+                addressesToReverse = []
+            }
+            const validReversedNames = reversedNames.map((n) => { return ethNamehash.normalize(n) === n ? n : '' })
+            addresses.forEach((key, i) => result[key] = validReversedNames[i])
+            return result
+        } catch (e) {
+            console.log(`Error in getDomainNames`, e)
+            return null
+        }
+    }
+
+    //Get reverse name of address. Returns null if user has not set it.
+    async getDomainName(address) {
+        try {
+            if (isAddress(address)) {
+                const checksummedAddress = toChecksumAddress(address)
+                const domain = await getReverseName(checksummedAddress, this.provider)
+                if (domain) {
+                    const addressRecord = await getAddressRecord(domain, this.provider)
+                    if (addressRecord && addressRecord === checksummedAddress) {
+                        return domain + ".dnero"
+                    }
+                }
+            }
+            return null
+        } catch (e) {
+            console.log(`Error in getDomainName`, e)
+            return null
+        }
+    }
+}
